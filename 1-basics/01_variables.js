cc8d4a28551c45f04fd1d1a60f04941a68e07d65
Harsh chandwani <harsh9425181074@gmail.com>
2025-08-26 21:57:02 +0000

    data types

--- 1-basics/01_variables.js
+++ 1-basics/01_variables.js
@@ -1,11 +1,22 @@
 const accountId = 144553
-let accounEmail = "harsh@gmail.com"
-var accountPassword  = "12345"
-accountCity = "jaipur"
-let accountState
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
 
-accounEmail = "hcnoneof"
-accountPassword = "44654"
-accountCity = "bengaluru"
+// accountId = 2 // not allowed // const can not be chnaged//
 
-console.table([accounEmail , accountPassword  ,accountCity , accountState])
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
