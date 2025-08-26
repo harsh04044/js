d2ec2ca7df0539b1c3105002850993cc404458f8
Harsh chandwani <harsh9425181074@gmail.com>
2025-08-26 21:35:42 +0000

    vairables

---
+++ 1-basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accounEmail = "harsh@gmail.com"
+var accountPassword  = "12345"
+accountCity = "jaipur"
+let accountState
+
+accounEmail = "hcnoneof"
+accountPassword = "44654"
+accountCity = "bengaluru"
+
+console.table([accounEmail , accountPassword  ,accountCity , accountState])
